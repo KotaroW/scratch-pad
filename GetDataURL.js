8f77d51dec384cd85acd3b3590e2e2e60c1b66f5
KotaroW <kotarochin@gmail.com>
2015-06-19 16:42:57 +1200

    Update GetDataURL.js

--- GetDataURL.js
+++ GetDataURL.js
@@ -1,9 +1,18 @@
+/********************************************************************************
+ * file: GetDataURL.js
+ * description:
+ *    generates the data url for an image file.
+ ********************************************************************************/
+
+/* function factory */
 var DataURLFactory = (function (f) {
   var file = f;
+  // add or remove image extensions as needed.
   var allowedExtensions = ["jpg", "jpeg", "png", "gif"];
   return {
     checkFileType : function () {
       var filename = f.name;
+      /* a straight-forward reg expression. */
       for (var iii = 0; iii < allowedExtensions.length; iii++) {
         var pattern = new RegExp("^.+\." + allowedExtensions[iii] + "$", "i");
         if (pattern.test(filename))
@@ -14,6 +23,7 @@ var DataURLFactory = (function (f) {
     getDataURL : function () {
       var reader = new FileReader();
       reader.onload = function (evt) {
+        /* the result could be displayed by alert or textarea. */
         prompt ("Data URL", evt.target.result);
       };
       reader.readAsDataURL(file);
@@ -21,13 +31,17 @@ var DataURLFactory = (function (f) {
   };
 });
 
+/* this function may be invoked by any events you wish.  */
 function getFile() {
   var files = document.querySelector("input[type=file]").files;
+  /* check to see if at least one file has been selected. */
   if (!files.length) {
     alert ("Please select a file");
     return;
   }
+  /* if there is a file, call the function factory. */
   var dataURLGetter = DataURLFactory(files[0]);
+  /* only image files are acceptable. */
   if (dataURLGetter.checkFileType()) {
     dataURLGetter.getDataURL();
   }
